81880a443f94c1eb3144a52554faba0bb6fd0b5a
mguduri <mguduri@manh.com>
2016-03-14 01:15:06 +0530

    Merge Sort First Commit

---
+++ src/com/mani/gayi/utils/UtilityClass.java
@@ -0,0 +1,22 @@
+package com.mani.gayi.utils;
+
+public class UtilityClass {
+
+	// print the array data to the console...
+	public void printArrayData(int array[]) {
+		System.out.print(": ");
+		for (int element : array) {
+			System.out.print(element + " : ");
+		}
+		System.out.println("");
+	}
+
+	// public static void main(String[] args) {
+	// int a[] = {10,230,303,200};
+	//
+	// UtilityClass u = new UtilityClass();
+	//
+	// u.printArrayData(a);
+	// }
+
+}
