5afd23cec2fe687fa8e7d2109ffa6e1029a173cc
mguduri <mguduri@manh.com>
2016-03-14 12:27:04 +0530

    Internationalization adding 2 properties files and using that

---
+++ src/com/mani/gayi/i18n/I18NDemo.java
@@ -0,0 +1,23 @@
+package com.mani.gayi.i18n;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class I18NDemo {
+	
+	
+	
+	 public static void main(String[] args) {  
+	  
+	  ResourceBundle bundle = ResourceBundle.getBundle("MessageBundle", Locale.US);  
+	  System.out.println("Message in "+Locale.US +":"+bundle.getString("greeting"));  
+	  
+	  //changing the default locale to indonasian   
+	  Locale.setDefault(new Locale("in", "ID"));  
+	  bundle = ResourceBundle.getBundle("MessageBundle");  
+	  System.out.println("Message in "+Locale.getDefault()+":"+bundle.getString("greeting"));  
+	  
+	  
+	}  
+
+}
