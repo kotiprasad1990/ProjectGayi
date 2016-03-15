a4e6c290f8bb434bda2a61bcf9a1a5ea29c93229
Gayi <lakshmi.bhallamudi4@gmail.com>
2016-03-15 00:22:10 -0700

    Commiting the beggining

---
+++ src/com/mani/gayi/basics/ForLoop.java
@@ -0,0 +1,17 @@
+package com.mani.gayi.basics;
+
+public class ForLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		for(int index=0;index<10000;index++)
+		{
+			System.out.println("Sri Rama ");
+		}
+		
+	}
+
+}
