6dfd3f915cd3c7b3ac92b561c7d5bead65468f2a
mguduri <manidhar.guduri@gmail.com>
2016-07-22 16:00:05 +0530

    Google Question: i18n (where 18 stands for the number of letters between the first i and the last n in the word “internationalization,”) Wiki it.
    Generate all such possible i18n strings for any given string. for eg.
    "careercup"=>"c7p","ca6p","c6up","car5p","ca5up","care4p","car4up","caree3p","care3up"..till
    the count is 0 which means its the complete string again.

---
+++ src/com/mani/gayi/string/String18Problem.java
@@ -0,0 +1,38 @@
+package com.mani.gayi.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class String18Problem {
+	
+	
+	public List<String> compress(String word)
+	{
+		List<String> wordslist = new ArrayList<String>();
+		
+		if(word.length()<2){
+			wordslist.add(word);
+			return wordslist;
+		}
+		 
+		for(int i=1; i<=word.length()-2; i++) {  
+		        for(int j=1; i+j<word.length(); j++) {  
+//Debugging statement System.out.println("word "+word.substring(0,j)+" i : "+i+" word.substring(i+j) " +word.substring(i+j));
+		        	wordslist.add(word.substring(0,j)+i+word.substring(i+j));  
+		        }  
+		        System.out.println("------------");
+		    }  
+		    return wordslist;  
+		
+	}
+
+	public static void main(String[] args) {
+		
+	List<String> words = new String18Problem().compress("Manidhar");
+//	for(String word : words)
+//	{
+//		System.out.println("words "+word);
+//	}
+	}
+	
+}
